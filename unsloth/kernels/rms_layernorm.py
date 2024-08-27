bff30a52ee9d3a73493819d2265770417436bc3b
Daniel Han <danielhanchen@gmail.com>
2024-08-26 20:27:18 -0700

    Update rms_layernorm.py

--- unsloth/kernels/rms_layernorm.py
+++ unsloth/kernels/rms_layernorm.py
@@ -1,17 +1,3 @@
-# Copyright 2023-present Daniel Han-Chen & the Unsloth team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import triton
 import triton.language as tl
 import torch
@@ -20,11 +6,16 @@ from .utils import calculate_settings
 
 @triton.jit
 def _rms_layernorm_forward(
-    Y, Y_row_stride,
-    X, X_row_stride,
-    W, W_row_stride,
-    r, r_row_stride,
-    n_cols, eps,
+    Y,
+    Y_row_stride,
+    X,
+    X_row_stride,
+    W,
+    W_row_stride,
+    r,
+    r_row_stride,
+    n_cols,
+    eps,
     BLOCK_SIZE : tl.constexpr
 ):
     """
@@ -32,6 +23,7 @@ def _rms_layernorm_forward(
         Inspiration from a Triton tutorial:
         https://triton-lang.org/main/getting-started/tutorials/05-layer-norm.html
     """
+
     row_idx = tl.program_id(0)
     col_offsets = tl.arange(0, BLOCK_SIZE)
     mask = col_offsets < n_cols
@@ -40,29 +32,36 @@ def _rms_layernorm_forward(
     X += row_idx * X_row_stride
     r += row_idx * r_row_stride
 
-    X_row = tl.load(X + col_offsets, mask = mask, other = 0).to(tl.float32)
-    W_row = tl.load(W + col_offsets, mask = mask, other = 0)#.to(tl.float32)
+    X_row = tl.load(X + col_offsets, mask=mask, other=0).to(tl.float32)
+    W_row = tl.load(W + col_offsets, mask=mask, other=0)#.to(tl.float32)
 
-    row_var = tl.sum(X_row * X_row, axis = 0) / n_cols
+    row_var = tl.sum(X_row * X_row, axis=0) / n_cols
     inv_var = tl.math.rsqrt(row_var + eps)
+
     tl.store(r, inv_var)
+
     normed = X_row * inv_var
     normed = normed.to(W_row.dtype) # Exact copy from HF
+    
     output = normed * W_row
-    tl.store(Y + col_offsets, output, mask = mask)
-pass
+    tl.store(Y + col_offsets, output, mask=mask)
 
 
-@triton.heuristics({"GEMMA": lambda args: args["GEMMA"],})
+
 @triton.jit
 def _rms_layernorm_backward(
-    dY, dY_row_stride,
-    X,   X_row_stride,
-    W,   W_row_stride,
-    r,   r_row_stride,
-    dW, dW_row_stride,
-    n_cols, eps,
-    GEMMA      : tl.constexpr,
+    dY,
+    dY_row_stride,
+    X,
+    X_row_stride,
+    W,
+    W_row_stride,
+    r,
+    r_row_stride,
+    dW,
+    dW_row_stride,
+    n_cols,
+    eps,
     BLOCK_SIZE : tl.constexpr,
 ):
     """
@@ -78,53 +77,22 @@ def _rms_layernorm_backward(
     X  += row_idx *  X_row_stride
     r  += row_idx *  r_row_stride
 
-    dY_row = tl.load(dY + col_offsets, mask = mask, other = 0).to(tl.float32)
-    X_row  = tl.load(X  + col_offsets, mask = mask, other = 0).to(tl.float32)
-    W_row  = tl.load(W  + col_offsets, mask = mask, other = 0).to(tl.float32)
+    dY_row = tl.load(dY + col_offsets, mask=mask, other=0).to(tl.float32)
+    X_row  = tl.load(X  + col_offsets, mask=mask, other=0).to(tl.float32)
+    W_row  = tl.load(W  + col_offsets, mask=mask, other=0).to(tl.float32)
 
     # Get saved row variance
     inv_var = tl.load(r).to(tl.float32)
+
+    dY_W = dY_row * W_row # < Liger added
     normed = X_row * inv_var
 
-    if GEMMA: dY_W = dY_row * (W_row + 1.0)
-    else:     dY_W = dY_row * W_row
-
-    rowsum_dY_normed = tl.sum(dY_W * normed, axis = 0)
+    rowsum_dY_normed = tl.sum(dY_W * normed, axis=0)
     output = inv_var/n_cols * (n_cols*dY_W - normed*rowsum_dY_normed)
-    tl.store(dY + col_offsets, output, mask = mask)
-pass
+    tl.store(dY + col_offsets, output, mask=mask)
 
-
-@triton.jit
-def _gemma_rms_layernorm_forward(
-    Y, Y_row_stride,
-    X, X_row_stride,
-    W, W_row_stride,
-    r, r_row_stride,
-    n_cols, eps,
-    BLOCK_SIZE : tl.constexpr,
-):
-    # Copies https://github.com/google-deepmind/gemma/blob/main/gemma/layers.py#L31
-    # and https://github.com/keras-team/keras-nlp/blob/v0.8.2/keras_nlp/models/gemma/rms_normalization.py#L33
-    # exactly. Essentially all in float32!
-    row_idx = tl.program_id(0)
-    col_offsets = tl.arange(0, BLOCK_SIZE)
-    mask = col_offsets < n_cols
-
-    Y += row_idx * Y_row_stride
-    X += row_idx * X_row_stride
-    r += row_idx * r_row_stride
-
-    X_row = tl.load(X + col_offsets, mask = mask, other = 0).to(tl.float32)
-    W_row = tl.load(W + col_offsets, mask = mask, other = 0).to(tl.float32)
-
-    row_var = tl.sum(X_row * X_row, axis = 0) / n_cols
-    inv_var = tl.math.rsqrt(row_var + eps)
-    tl.store(r, inv_var)
-    normed = X_row * inv_var
-    output = normed * (W_row + 1.0)
-
-    tl.store(Y + col_offsets, output, mask = mask)
+    # calculate the gradient of W
+    tl.store(dW_ptr + col_offsets, dY_normed, mask=mask) < Liger added
 pass
 
 
@@ -137,23 +105,27 @@ class Fast_RMS_Layernorm(torch.autograd.Function):
         n_rows, n_cols = X.shape
         BLOCK_SIZE, num_warps = calculate_settings(n_cols)
 
-        Y = torch.empty((n_rows, n_cols), dtype = X.dtype, device = "cuda:0")
-        r = torch.empty(n_rows, dtype = torch.float32, device = "cuda:0")
+        Y = torch.empty((n_rows, n_cols), dtype=X.dtype, device="cuda:0")
+        r = torch.empty(n_rows, dtype=torch.float32, device="cuda:0")
 
-        fx = _gemma_rms_layernorm_forward if gemma else _rms_layernorm_forward
-        fx[(n_rows,)](
-            Y, Y.stride(0),
-            X, X.stride(0),
-            W, W.stride(0),
-            r, r.stride(0),
-            n_cols, eps,
-            BLOCK_SIZE = BLOCK_SIZE,
-            num_warps  = num_warps,
+        _rms_layernorm_forward[(n_rows,)](
+            Y,
+            Y.stride(0),
+            X,
+            X.stride(0),
+            W,
+            W.stride(0),
+            r,
+            r.stride(0),
+            n_cols,
+            eps,
+            BLOCK_SIZE=BLOCK_SIZE,
+            num_warps=num_warps,
         )
         ctx.eps = eps
         ctx.BLOCK_SIZE = BLOCK_SIZE
-        ctx.num_warps  = num_warps
-        ctx.GEMMA = gemma
+        ctx.num_warps = num_warps
+
         ctx.save_for_backward(X, W, r)
         return Y.view(*shape)
     pass
@@ -168,15 +140,20 @@ class Fast_RMS_Layernorm(torch.autograd.Function):
         dW = X
 
         _rms_layernorm_backward[(n_rows,)](
-            dY, dY.stride(0),
-            X,  X .stride(0),
-            W,  W .stride(0),
-            r,  r .stride(0),
-            dW, dW.stride(0),
-            n_cols, ctx.eps,
-            GEMMA      = ctx.GEMMA,
-            BLOCK_SIZE = ctx.BLOCK_SIZE,
-            num_warps  = ctx.num_warps,
+            dY,
+            dY.stride(0),
+            X,
+            X .stride(0),
+            W,
+            W .stride(0),
+            r,
+            r .stride(0),
+            dW,
+            dW.stride(0),
+            n_cols,
+            ctx.eps,
+            BLOCK_SIZE=ctx.BLOCK_SIZE,
+            num_warps=ctx.num_warps,
         )
         dX = dY.view(*shape)
         return dX, None, None, None
