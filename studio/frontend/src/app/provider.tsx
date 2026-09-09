505c83ba491b6ac681af5e96cf32cc3206c17e88
Daniel Han <danielhanchen@gmail.com>
2026-09-09 09:45:28 +0000

    Studio: offer a prepared update in the banner and settings

--- studio/frontend/src/app/provider.tsx
+++ studio/frontend/src/app/provider.tsx
@@ -408,6 +408,7 @@ function TauriUpdateLayer({
       <UpdateBanner
         status={update.status}
         info={update.info}
+        preparation={update.preparation}
         dismissed={update.dismissed}
         lastFailure={update.lastFailure}
         isExternalServer={isExternalServer}
